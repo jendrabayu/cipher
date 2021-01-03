e96a4df2b61286d5236c66f913b0fe8a94f3a320
Jendra Bayu Nugraha <jendra455@gmail.com>
2021-01-03 21:57:42 +0700

    Update

--- src/cipher/helper.js
+++ src/cipher/helper.js
@@ -4,9 +4,6 @@ exports.mod = (n, m) => {
   return ((n % m) + m) % m;
 };
 
-exports.div = (n, m) => {
-  return Math.floor(n / m);
-};
 
 // Tests whether the specified character code is an uppercase letter.
 exports.isUppercase = (c) => {
@@ -59,38 +56,4 @@ exports.gcd = (a, b) => {
     return a;
   }
   return this.gcd(b, a % b);
-};
-
-exports.modularInverse = (m, n) => {
-  m = this.mod(m, n);
-  for (let x = 1; x < n; x++) if ((m * x) % n == 1) return x;
-};
-
-exports.inverseMatrix = (matrix) => {
-  let minorMatrix = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      minorMatrix[i][j] =
-        matrix[(i + 1) % 3][(j + 1) % 3] * matrix[(i + 2) % 3][(j + 2) % 3] -
-        matrix[(i + 1) % 3][(j + 2) % 3] * matrix[(i + 2) % 3][(j + 1) % 3];
-    }
-  }
-
-  let adjointMatrix = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
-
-  for (let i = 0; i < minorMatrix.length; i++) {
-    for (let j = 0; j < minorMatrix[i].length; j++) {
-      adjointMatrix[j][i] = minorMatrix[i][j];
-    }
-  }
-  return adjointMatrix;
 };
